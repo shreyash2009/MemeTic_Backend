2d1e7c8205bf1f97df31b445ca7ba0a17137feda
shreyashe <shreyash.22010681@viit.ac.in>
2023-01-28 23:18:24 +0530

    Made some changes with url

--- index.js
+++ index.js
@@ -7,11 +7,17 @@ import memeRouter from './Routes/Meme.js'
 import dotenv from 'dotenv'
 const app = express();
 
-dotenv.config();  //you will able to access the value defined in .env file
-
+// dotenv.config(); //you will able to access the value defined in .env file
+dotenv.config({path:'config.env'});
 app.use(morgan("dev"));
-app.use(express.json({limit: "30mb", extended:true}));
-app.use(express.urlencoded({limit: "30mb", extended:true}));
+app.use(express.json({
+    limit: "30mb",
+    extended: true
+}));
+app.use(express.urlencoded({
+    limit: "30mb",
+    extended: true
+}));
 app.use(cors());
 
 app.use("/users/", userRouter); //http://localhost:5000/users/signup
@@ -19,13 +25,16 @@ app.use("/meme/", memeRouter); //http://localhost:5000/meme
 
 
 const PORT = process.env.PORT || 5000;
-
-mongoose.connect(process.env.MONGODB_URL).then(()=>{
-    app.listen(PORT, ()=>{
+mongoose.connect("mongodb+srv://Admin:Password@cluster0.twozgvj.mongodb.net/Blog_app?retryWrites=true&w=majority", {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+}).then(() => {
+    app.listen(PORT, () => {
         console.log(`server is running on port ${PORT}`)
     })
-}).catch(error => { console.log(error)})
-app.get("/", (req, res)=>{
+}).catch(error => {
+    console.log(error)
+})
+app.get("/", (req, res) => {
     res.send("HEllo express");
 })
-
